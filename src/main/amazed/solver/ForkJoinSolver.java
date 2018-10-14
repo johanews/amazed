ff68451252407637e61896af1d1568f419a9a44d
Johan Sjöberg <johasjob@student.chalmers.se>
2018-10-15 00:36:41 +0200

    Save first draft

--- src/main/amazed/solver/ForkJoinSolver.java
+++ src/main/amazed/solver/ForkJoinSolver.java
@@ -2,13 +2,8 @@ package amazed.solver;
 
 import amazed.maze.Maze;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Set;
-
-import java.util.concurrent.ForkJoinPool;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
 
 /**
@@ -22,6 +17,18 @@ import java.util.concurrent.ConcurrentSkipListSet;
 
 public class ForkJoinSolver extends SequentialSolver {
     private static final long serialVersionUID = 1L;
+
+    private static Set<Integer> visited = new ConcurrentSkipListSet<>();
+    private static Map<Integer, Integer> predecessor = new ConcurrentHashMap<>();
+    private static boolean finished = false;
+
+    private List<ForkJoinSolver> subtasks = new ArrayList<>();
+    private int outset = start;
+
+    public ForkJoinSolver(Maze maze, int forkAfter, int outset) {
+        this(maze, forkAfter);
+        this.outset = outset;
+    }
 
     /**
      * Creates a solver that searches in <code>maze</code> from the start node to a
@@ -38,11 +45,11 @@ public class ForkJoinSolver extends SequentialSolver {
      * start node to a goal, forking after a given number of visited
      * nodes.
      *
-     * @param maze        the maze to be searched
-     * @param forkAfter   the number of steps (visited nodes) after
-     *                    which a parallel task is forked; if
-     *                    <code>forkAfter &lt;= 0</code> the solver never
-     *                    forks new tasks
+     * @param maze      the maze to be searched
+     * @param forkAfter the number of steps (visited nodes) after
+     *                  which a parallel task is forked; if
+     *                  <code>forkAfter &lt;= 0</code> the solver never
+     *                  forks new tasks
      */
     public ForkJoinSolver(Maze maze, int forkAfter) {
         this(maze);
@@ -56,9 +63,9 @@ public class ForkJoinSolver extends SequentialSolver {
      * goals, or all goals are unreachable), the method returns
      * <code>null</code>.
      *
-     * @return   the list of node identifiers from the start node to a
-     *           goal node in the maze; <code>null</code> if such a path cannot
-     *           be found.
+     * @return the list of node identifiers from the start node to a
+     * goal node in the maze; <code>null</code> if such a path cannot
+     * be found.
      */
     @Override
     public List<Integer> compute() {
@@ -66,7 +73,65 @@ public class ForkJoinSolver extends SequentialSolver {
     }
 
     private List<Integer> parallelSearch() {
+        int player = maze.newPlayer(outset);
+        frontier.push(outset);
+
+        while (!frontier.empty() && !finished) {
+            int current = frontier.pop();
+
+            if (maze.hasGoal(current)) {
+                maze.move(player, current);
+                super.predecessor = predecessor;
+                finished = true;
+                return pathFromTo(start, current);
+            }
+
+            maze.move(player, current);
+            visited.add(current);
+
+            Iterator<Integer> it = unvisited(current).iterator();
+
+            if (it.hasNext())
+                progress(current, it.next());
+            if (it.hasNext()) {
+                createTasks(current, it);
+            }
+        }
+        return join_tasks();
+    }
+
+    private List<Integer> join_tasks() {
+        for (ForkJoinSolver st: subtasks) {
+            List<Integer> sp = st.join();
+            if (sp != null)
+                return sp;
+        }
         return null;
     }
-    
+
+    private void createTasks(int current, Iterator<Integer> it) {
+        ForkJoinSolver task;
+        while (it.hasNext()) {
+            int node = it.next();
+            task = new ForkJoinSolver(maze, forkAfter, node);
+            subtasks.add(task);
+            predecessor.put(node, current);
+            task.fork();
+        }
+    }
+
+    private void progress(int current, int next) {
+        frontier.push(next);
+        predecessor.put(next, current);
+    }
+
+    private Set<Integer> unvisited(int current) {
+        Set<Integer> result = new HashSet<>();
+        for (int nb : maze.neighbors(current)) {
+            if (!visited.contains(nb))
+                result.add(nb);
+        }
+        return result;
+    }
+
 }
