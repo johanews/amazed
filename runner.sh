01a0b89ee088f074db2778b7ff50be2cf3345704
noone <joel.roxell@annevo.se>
2018-10-22 21:57:53 +0200

    runner: added test setup.

--- runner.sh
+++ runner.sh
@@ -1,32 +1,62 @@
 #!/bin/bash
 
-COUNTER=0
-REACH=30
+# MODE 0 == Fork after; 1 == Fork at each branch
+# SHOULD_WAIT - true|false if the parrent process should wait on forked threads before it continues.
+# POOL_SIZE int - workers in the pool
 
-while [ $COUNTER -lt $REACH ]; do
-   echo "fork on 3d parallel-3 -1"
-   echo $COUNTER
-   MODE=0 SHOULD_WAIT=false java -cp src/main amazed.Main maps/small.map parallel-3 -1
-   echo "-----"
-   COUNTER=$(( $COUNTER + 1 ))
+REACH=10
+
+function run {
+    settings=(1 2 4 8)
+    counter=0
+    
+    for pool_size in "${settings[@]}"
+    do  
+        echo "${1} on map ${2} with pool size:${pool_size} wait: ${4}"
+
+        while [ $counter -lt $REACH ]; do
+            POOL_SIZE=${pool_size} MODE=$3 SHOULD_WAIT=$4 java -cp src/main amazed.Main $2 $1 -1
+            
+            counter=$(( $counter + 1 ))
+        done
+        counter=0
+    done  
+}
+
+maps=(small medium large huge)
+
+for name in "${maps[@]}"
+do
+    echo "Running: ${name}"
+
+    echo "parallel ${name}.map branch with wait"
+    run parallel-3 maps/${name}.map 1 true
+    echo "------------------------------------"
+
+    echo "parallel-3 ${name}.map fork after"
+    run parallel-3 maps/${name}.map 0 false
+    echo "------------------------------------"
+
+    echo "parallel-3 ${name}.map branch with wait"
+    run parallel-3 maps/${name}.map 1 true
+    echo "------------------------------------"
+
+    echo "parallel-9 ${name}.map fork after"
+    run parallel-9 maps/${name}.map 0 false
+    echo "------------------------------------"
+
+    echo "parallel-9 ${name}.map fork after with wait"
+    run parallel-9 maps/${name}.map 0 true
+    echo "------------------------------------"
+
+    echo "parallel ${name}.map branch"
+    run parallel-3 maps/${name}.map 1 false
+    echo "------------------------------------"
+
+    echo "parallel ${name}.map branch with wait"
+    run parallel-3 maps/${name}.map 1 true
+    echo "------------------------------------"
 done
 
-COUNTER=0
+exit 0
 
-while [ $COUNTER -lt $REACH ]; do
-   echo "fork on 9th parallel-9 -1"
-   echo $COUNTER
-   MODE=0 SHOULD_WAIT=false java -cp src/main amazed.Main maps/small.map parallel-9 -1
-   echo "-----"
-   COUNTER=$(( $COUNTER + 1 ))
-done
-
-COUNTER=0
-
-while [ $COUNTER -lt $REACH ]; do
-   echo "branch parallel-3 -1"
-   echo $COUNTER
-   MODE=1 SHOULD_WAIT=false java -cp src/main amazed.Main maps/small.map parallel-9 -1
-   echo "-----"
-   COUNTER=$(( $COUNTER + 1 ))
-done
